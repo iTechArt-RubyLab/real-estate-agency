beeb07291b2c85a4ecb9f9597e1e9034fd0f94b3
Sasha Karol <70864999+WhatWasThatt@users.noreply.github.com>
2021-12-25 00:23:37 +0300

    RSA-002/add devise (#2)
    * RSA-002/install devise
    
    * RSA-002/customization devise
    
    * RSA-002/add home controller
    
    * RSA-002/add user model with annotates
    
    * RSA-002/add users views
    
    * RSA-002/add trackable and confirmable to user
    
    * RSA-002/add mailcatcher

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
+  root to: 'home#index'
 end
