366eebb50c0cff3c4e063f8850586397123832b5
WhatWasThatt <sashakorol584@mail.ru>
2022-01-05 12:25:30 +0200

    RSA-023/add tests for flats

--- spec/factories/flats.rb
+++ spec/factories/flats.rb
@@ -26,15 +26,353 @@
 #  fk_rails_...  (wall_material_id => wall_materials.id)
 #
 FactoryBot.define do
-  factory :flat do
-    rooms_count { 1 }
-    floor { 1 }
-    year_of_construction { 1 }
-    celling_height { '9.99' }
-    total_area { '9.99' }
-    living_area { '9.99' }
-    kitchen_area { '9.99' }
-    renovation { nil }
-    wall_material { nil }
+  factory :rooms_count, class: Flat do
+    trait :valid do
+      association :renovation
+      association :wall_material
+      rooms_count { Faker::Number.within(range: 1..40) }
+      floor { Faker::Number.within(range: 1..34) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_big do
+      association :renovation
+      association :wall_material
+      rooms_count { Faker::Number.within(range: 41..100) }
+      floor { Faker::Number.within(range: 1..34) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_negative do
+      association :renovation
+      association :wall_material
+      rooms_count { Faker::Number.negative }
+      floor { Faker::Number.within(range: 1..34) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_with_letters do
+      association :renovation
+      association :wall_material
+      rooms_count { Faker::Movies::LordOfTheRings.character }
+      floor { Faker::Number.within(range: 1..34) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+  end
+
+  factory :floor_f, class: Flat do
+    trait :valid do
+      association :renovation
+      association :wall_material
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_big do
+      association :renovation
+      association :wall_material
+      floor { Faker::Number.within(range: 35..100) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_negative do
+      association :renovation
+      association :wall_material
+      floor { Faker::Number.negative }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_with_letters do
+      association :renovation
+      association :wall_material
+      floor { Faker::Movies::LordOfTheRings.character }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+  end
+
+  factory :year_of_construction_f, class: Flat do
+    trait :valid do
+      association :renovation
+      association :wall_material
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_big do
+      association :renovation
+      association :wall_material
+      year_of_construction { Faker::Number.within(range: 2023..2100) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_negative do
+      association :renovation
+      association :wall_material
+      year_of_construction { Faker::Number.negative }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_with_letters do
+      association :renovation
+      association :wall_material
+      year_of_construction { Faker::Movies::LordOfTheRings.character }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+  end
+
+  factory :celling_height, class: Flat do
+    trait :valid do
+      association :renovation
+      association :wall_material
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_big do
+      association :renovation
+      association :wall_material
+      celling_height { Faker::Number.within(range: 5.1..10.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_negative do
+      association :renovation
+      association :wall_material
+      celling_height { Faker::Number.negative }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_with_letters do
+      association :renovation
+      association :wall_material
+      celling_height { Faker::Movies::LordOfTheRings.character }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+  end
+
+  factory :total_area_f, class: Flat do
+    trait :valid do
+      association :renovation
+      association :wall_material
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_big do
+      association :renovation
+      association :wall_material
+      total_area { Faker::Number.within(range: 1000.1..2000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_negative do
+      association :renovation
+      association :wall_material
+      total_area { Faker::Number.negative }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_with_letters do
+      association :renovation
+      association :wall_material
+      total_area { Faker::Movies::LordOfTheRings.character }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+  end
+
+  factory :living_area, class: Flat do
+    trait :valid do
+      association :renovation
+      association :wall_material
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_big do
+      association :renovation
+      association :wall_material
+      living_area { Faker::Number.within(range: 100.1..200.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_negative do
+      association :renovation
+      association :wall_material
+      living_area { Faker::Number.negative }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+
+    trait :invalid_with_letters do
+      association :renovation
+      association :wall_material
+      living_area { Faker::Movies::LordOfTheRings.character }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+    end
+  end
+
+  factory :kitchen_area, class: Flat do
+    trait :valid do
+      association :renovation
+      association :wall_material
+      kitchen_area { Faker::Number.within(range: 2.0..100.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+    end
+
+    trait :invalid_big do
+      association :renovation
+      association :wall_material
+      kitchen_area { Faker::Number.within(range: 100.1..200.0) }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+    end
+
+    trait :invalid_negative do
+      association :renovation
+      association :wall_material
+      kitchen_area { Faker::Number.negative }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+    end
+
+    trait :invalid_with_letters do
+      association :renovation
+      association :wall_material
+      kitchen_area { Faker::Movies::LordOfTheRings.character }
+      living_area { Faker::Number.within(range: 2.0..100.0) }
+      total_area { Faker::Number.within(range: 2.0..1000.0) }
+      celling_height { Faker::Number.within(range: 1.80..5.0) }
+      year_of_construction { Faker::Number.within(range: 1900..2022) }
+      floor { Faker::Number.within(range: 1..34) }
+      rooms_count { Faker::Number.within(range: 1..40) }
+    end
   end
 end
--- spec/models/flat_spec.rb
+++ spec/models/flat_spec.rb
@@ -28,5 +28,164 @@
 require 'rails_helper'
 
 RSpec.describe Flat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create flat rooms count' do
+    let(:valid) { build :rooms_count, :valid }
+    let(:invalid_big) { build :rooms_count, :invalid_big }
+    let(:invalid_negative) { build :rooms_count, :invalid_negative }
+    let(:invalid_with_letters) { build :rooms_count, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big rooms count' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative rooms count' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when rooms count with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
+
+  describe 'create flat floor' do
+    let(:valid) { build :floor_f, :valid }
+    let(:invalid_big) { build :floor_f, :invalid_big }
+    let(:invalid_negative) { build :floor_f, :invalid_negative }
+    let(:invalid_with_letters) { build :floor_f, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big floor' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative floor' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when floor with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
+
+  describe 'create flat year of construction' do
+    let(:valid) { build :year_of_construction_f, :valid }
+    let(:invalid_big) { build :year_of_construction_f, :invalid_big }
+    let(:invalid_negative) { build :year_of_construction_f, :invalid_negative }
+    let(:invalid_with_letters) { build :year_of_construction_f, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big year of construction' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative year of construction' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when year of construction with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
+
+  describe 'create flat celling height' do
+    let(:valid) { build :celling_height, :valid }
+    let(:invalid_big) { build :celling_height, :invalid_big }
+    let(:invalid_negative) { build :celling_height, :invalid_negative }
+    let(:invalid_with_letters) { build :celling_height, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big celling height' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative celling height' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when celling height with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
+
+  describe 'create flat total area' do
+    let(:valid) { build :total_area_f, :valid }
+    let(:invalid_big) { build :total_area_f, :invalid_big }
+    let(:invalid_negative) { build :total_area_f, :invalid_negative }
+    let(:invalid_with_letters) { build :total_area_f, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big total area' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative total area' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when total area with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
+
+  describe 'create flat living area' do
+    let(:valid) { build :living_area, :valid }
+    let(:invalid_big) { build :living_area, :invalid_big }
+    let(:invalid_negative) { build :living_area, :invalid_negative }
+    let(:invalid_with_letters) { build :living_area, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big living area' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative living area' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when living area with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
+
+  describe 'create flat kitchen area' do
+    let(:valid) { build :kitchen_area, :valid }
+    let(:invalid_big) { build :kitchen_area, :invalid_big }
+    let(:invalid_negative) { build :kitchen_area, :invalid_negative }
+    let(:invalid_with_letters) { build :kitchen_area, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big kitchen area' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative kitchen area' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when kitchen area with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
 end
