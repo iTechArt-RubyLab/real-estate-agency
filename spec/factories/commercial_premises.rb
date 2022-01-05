08009655ad2f0a306ecdf10dcfcf2f2cb24f6fe4
WhatWasThatt <sashakorol584@mail.ru>
2022-01-05 10:15:53 +0200

    RSA-023/add tests for commercial premises

--- spec/factories/commercial_premises.rb
+++ spec/factories/commercial_premises.rb
@@ -20,11 +20,139 @@
 #  fk_rails_...  (commercial_premises_kind_id => commercial_premises_kinds.id)
 #
 FactoryBot.define do
-  factory :commercial_premise do
-    area { '9.99' }
-    floor { 1 }
-    number_of_premises { 1 }
-    plot_of_land { 1 }
-    commercial_premises_kind { nil }
+  factory :area, class: CommercialPremise do
+    trait :valid do
+      association :commercial_premises_kind
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+
+    trait :invalid_big do
+      association :commercial_premises_kind
+      area { Faker::Number.within(range: 1000.1..2000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+
+    trait :invalid_negative do
+      association :commercial_premises_kind
+      area { Faker::Number.negative }
+      floor { Faker::Number.within(range: 1..34) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+
+    trait :invalid_with_letters do
+      association :commercial_premises_kind
+      area { Faker::Movies::LordOfTheRings.character }
+      floor { Faker::Number.within(range: 1..34) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+  end
+
+  factory :floor, class: CommercialPremise do
+    trait :valid do
+      association :commercial_premises_kind
+      floor { Faker::Number.within(range: 1..34) }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+
+    trait :invalid_big do
+      association :commercial_premises_kind
+      floor { Faker::Number.within(range: 35..60) }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+
+    trait :invalid_negative do
+      association :commercial_premises_kind
+      floor { Faker::Number.negative }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+
+    trait :invalid_with_letters do
+      association :commercial_premises_kind
+      floor { Faker::Movies::LordOfTheRings.character }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+  end
+
+  factory :number_of_premises, class: CommercialPremise do
+    trait :valid do
+      association :commercial_premises_kind
+      number_of_premises { Faker::Number.within(range: 1..100) }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+
+    trait :invalid_big do
+      association :commercial_premises_kind
+      number_of_premises { Faker::Number.within(range: 101..200) }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+
+    trait :invalid_negative do
+      association :commercial_premises_kind
+      number_of_premises { Faker::Number.negative }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+
+    trait :invalid_with_letters do
+      association :commercial_premises_kind
+      number_of_premises { Faker::Movies::LordOfTheRings.character }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+    end
+  end
+
+  factory :plot_of_land, class: CommercialPremise do
+    trait :valid do
+      association :commercial_premises_kind
+      plot_of_land { Faker::Number.within(range: 1..1000) }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+    end
+
+    trait :invalid_big do
+      association :commercial_premises_kind
+      plot_of_land { Faker::Number.within(range: 1001..2000) }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+    end
+
+    trait :invalid_negative do
+      association :commercial_premises_kind
+      plot_of_land { Faker::Number.negative }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+    end
+
+    trait :invalid_with_letters do
+      association :commercial_premises_kind
+      plot_of_land { Faker::Movies::LordOfTheRings.character }
+      area { Faker::Number.within(range: 0.1..1000.0) }
+      floor { Faker::Number.within(range: 1..34) }
+      number_of_premises { Faker::Number.within(range: 1..100) }
+    end
   end
 end
--- spec/models/commercial_premise_spec.rb
+++ spec/models/commercial_premise_spec.rb
@@ -22,5 +22,95 @@
 require 'rails_helper'
 
 RSpec.describe CommercialPremise, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create commercial premise area' do
+    let(:valid) { build :area, :valid }
+    let(:invalid_big) { build :area, :invalid_big }
+    let(:invalid_negative) { build :area, :invalid_negative }
+    let(:invalid_with_letters) { build :area, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big area' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative area' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when area with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
+
+  describe 'create commercial premise floor' do
+    let(:valid) { build :floor, :valid }
+    let(:invalid_big) { build :floor, :invalid_big }
+    let(:invalid_negative) { build :floor, :invalid_negative }
+    let(:invalid_with_letters) { build :floor, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big floor' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative floor' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when floor with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
+
+  describe 'create commercial premise number of premises' do
+    let(:valid) { build :number_of_premises, :valid }
+    let(:invalid_big) { build :number_of_premises, :invalid_big }
+    let(:invalid_negative) { build :number_of_premises, :invalid_negative }
+    let(:invalid_with_letters) { build :number_of_premises, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big number of premises' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative number of premises' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when number of premises with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
+
+  describe 'create commercial premise plot of land' do
+    let(:valid) { build :plot_of_land, :valid }
+    let(:invalid_big) { build :plot_of_land, :invalid_big }
+    let(:invalid_negative) { build :plot_of_land, :invalid_negative }
+    let(:invalid_with_letters) { build :plot_of_land, :invalid_with_letters }
+
+    context 'with valid attributes' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too big plot of lands' do
+      it { expect(invalid_big).not_to be_valid }
+    end
+
+    context 'when negative plot of land' do
+      it { expect(invalid_negative).not_to be_valid }
+    end
+
+    context 'when plot of land with letters' do
+      it { expect(invalid_with_letters).not_to be_valid }
+    end
+  end
 end
