320c2b94dad57108340f2799356122a432a8eba8
WhatWasThatt <sashakorol584@mail.ru>
2022-01-05 10:18:28 +0200

    RSA-023/add tests for commercial premises kinds

--- spec/models/commercial_premises_kind_spec.rb
+++ spec/models/commercial_premises_kind_spec.rb
@@ -10,5 +10,26 @@
 require 'rails_helper'
 
 RSpec.describe CommercialPremisesKind, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create commercial premises kind name' do
+    let(:valid) { build :commercial_premises_kind, :valid }
+    let(:invalid_long) { build :commercial_premises_kind, :invalid_long }
+    let(:invalid_short) { build :commercial_premises_kind, :invalid_short }
+    let(:invalid_with_numbers) { build :commercial_premises_kind, :invalid_with_numbers }
+
+    context 'with valid' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too long' do
+      it { expect(invalid_long).not_to be_valid }
+    end
+
+    context 'when too short' do
+      it { expect(invalid_short).not_to be_valid }
+    end
+
+    context 'when with numbers' do
+      it { expect(invalid_with_numbers).not_to be_valid }
+    end
+  end
 end
