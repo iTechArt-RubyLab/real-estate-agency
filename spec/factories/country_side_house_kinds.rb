5a5b9175bd20995390a1df9d4423572b4da236fc
WhatWasThatt <sashakorol584@mail.ru>
2022-01-05 10:19:18 +0200

    RSA-023/add tests for country side houses kinds

--- spec/factories/country_side_house_kinds.rb
+++ spec/factories/country_side_house_kinds.rb
@@ -8,7 +8,21 @@
 #  updated_at :datetime         not null
 #
 FactoryBot.define do
-  factory :country_side_house_kind do
-    name { 'MyString' }
+  factory :country_side_house_kind, class: CountrySideHouseKind do
+    trait :valid do
+      name { Faker::Lorem.characters(number: 10, min_alpha: 10) }
+    end
+
+    trait :invalid_long do
+      name { Faker::Lorem.characters(number: 31, min_alpha: 31) }
+    end
+
+    trait :invalid_short do
+      name { Faker::Lorem.characters(number: 2, min_alpha: 2) }
+    end
+
+    trait :invalid_with_numbers do
+      name { Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1) }
+    end
   end
 end
--- spec/models/country_side_house_kind_spec.rb
+++ spec/models/country_side_house_kind_spec.rb
@@ -10,5 +10,26 @@
 require 'rails_helper'
 
 RSpec.describe CountrySideHouseKind, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create country side house kind name' do
+    let(:valid) { build :country_side_house_kind, :valid }
+    let(:invalid_long) { build :country_side_house_kind, :invalid_long }
+    let(:invalid_short) { build :country_side_house_kind, :invalid_short }
+    let(:invalid_with_numbers) { build :country_side_house_kind, :invalid_with_numbers }
+
+    context 'with valid' do
+      it { expect(valid).to be_valid }
+    end
+
+    context 'when too long' do
+      it { expect(invalid_long).not_to be_valid }
+    end
+
+    context 'when too short' do
+      it { expect(invalid_short).not_to be_valid }
+    end
+
+    context 'when with numbers' do
+      it { expect(invalid_with_numbers).not_to be_valid }
+    end
+  end
 end
