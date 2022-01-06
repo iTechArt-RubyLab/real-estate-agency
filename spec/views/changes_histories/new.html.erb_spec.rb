fa309311dd40de5ed39521e29d15d17779e290b4
Sasha Karol <70864999+WhatWasThatt@users.noreply.github.com>
2022-01-06 18:02:23 +0300

    RSA-028/add default changes history (#20)

---
+++ spec/views/changes_histories/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "changes_histories/new", type: :view do
+  before(:each) do
+    assign(:changes_history, ChangesHistory.new(
+      changes: "MyString",
+      user: nil
+    ))
+  end
+
+  it "renders new changes_history form" do
+    render
+
+    assert_select "form[action=?][method=?]", changes_histories_path, "post" do
+
+      assert_select "input[name=?]", "changes_history[changes]"
+
+      assert_select "input[name=?]", "changes_history[user_id]"
+    end
+  end
+end
