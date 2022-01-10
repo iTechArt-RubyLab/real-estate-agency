c67c56d60c2c465d7da9578fd11fec182399cc4d
WhatWasThatt <sashakorol584@mail.ru>
2022-01-10 12:06:43 +0200

    RSA-042/add factory for role

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -18,8 +18,8 @@
 #  updated_at                       :datetime         not null
 #
 FactoryBot.define do
-  factory :role do
-    name { 'MyString' }
+  factory :role, class: Role do
+    name { Faker::Lorem.characters(number: 10, min_alpha: 10) }
     can_read_lot { false }
     can_edit_lot { false }
     can_asign { false }
@@ -30,5 +30,17 @@ FactoryBot.define do
     can_create_role { false }
     can_approve_realtor { false }
     can_freeze_user { false }
+    
+    trait :invalid_long do
+      name { Faker::Lorem.characters(number: 31, min_alpha: 31) }
+    end
+
+    trait :invalid_short do
+      name { Faker::Lorem.characters(number: 2, min_alpha: 2) }
+    end
+
+    trait :invalid_with_numbers do
+      name { Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1) }
+    end
   end
 end
