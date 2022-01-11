ef7af2da9a2e258662a1f8b9b50b229728b35a87
WhatWasThatt <sashakorol584@mail.ru>
2022-01-11 12:30:06 +0200

    RSA-044/remove bug routes

--- config/routes.rb
+++ config/routes.rb
@@ -28,9 +28,6 @@ Rails.application.routes.draw do
   resources :addresses
   resources :streets
   resources :districts
-  resources :users do
-    resources :roles
-  end
   resources :roles
   devise_for :users, controllers: { 
     omniauth_callbacks: 'omniauth',
