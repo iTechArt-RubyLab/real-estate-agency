f910bdbe77b137bfe443cd5dd5bfcb4b1f37d6ca
WhatWasThatt <sashakorol584@mail.ru>
2022-01-12 19:12:10 +0200

    RSA-045/add presenters to admin panel view

---
+++ app/presenters/admin_panel_presenter.rb
@@ -0,0 +1,13 @@
+class AdminPanelPresenter
+  def initialize(user)
+    @user = user
+  end
+
+  def email_without_domain_name
+    user.email.split('@').first
+  end
+
+  private
+
+  attr_accessor :user
+end
