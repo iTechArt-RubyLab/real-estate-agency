ce7bf5d38794528ca347aee8b0603c649a5f6153
WhatWasThatt <sashakorol584@mail.ru>
2022-01-14 10:10:41 +0200

    RSA-047/update create registration method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,13 +6,31 @@ module Users
     before_action :configure_account_update_params, only: [:update]
 
     def create
-      b = ClientProfile.new
-      b.save
-      a = build_resource(configure_sign_up_params)
-      a.profilable_id = b.id
-      a.profilable_type = 'ClientProfile'
-      binding.pry
-      super
+      if sign_up_params[:profilable_type] == 'ClientProfile'
+        client_profile = ClientProfile.create(sign_up_client_profile_params)
+        user_sign_up_params = sign_up_params.merge(profilable_id: client_profile.id)
+      else
+        realtor_profile = RealtorProfile.create(sign_up_realtor_profile_params)
+        user_sign_up_params = sign_up_params.merge(profilable_id: realtor_profile.id)
+      end
+      build_resource(user_sign_up_params)
+      resource.save
+      yield resource if block_given?
+      if resource.persisted?
+        if resource.active_for_authentication?
+          set_flash_message! :notice, :signed_up
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        resource.errors.full_messages.each { |x| flash[:notice] = x }
+      end
     end
 
     protected
