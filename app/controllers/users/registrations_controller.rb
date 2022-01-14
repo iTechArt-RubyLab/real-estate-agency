9b2179d308ac04e9df46d7a357cd02d58b75e44e
Sasha Karol <70864999+WhatWasThatt@users.noreply.github.com>
2022-01-14 12:31:18 +0300

    RSA-047/fix registration (#66)
    * RSA-047/update permit params
    
    * RSA-047/add rules for rubocop
    
    * RSA-047/update create registration method
    
    * RSA-047/add refistration form
    
    * RSA-047/add css styling
    
    * RSA-047/add file field and bootstrap classes
    
    * RSA-047/add avatar to permit params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,24 +6,49 @@ module Users
     before_action :configure_account_update_params, only: [:update]
 
     def create
-      b = ClientProfile.new
-      b.save
-      a = build_resource(configure_sign_up_params)
-      a.profilable_id = b.id
-      a.profilable_type = 'ClientProfile'
-      binding.pry
-      super
+      if sign_up_params[:profilable_type] == 'ClientProfile'
+        client_profile = ClientProfile.create(sign_up_client_profile_params)
+        user_sign_up_params = sign_up_params.merge(profilable_id: client_profile.id)
+      else
+        realtor_profile = RealtorProfile.create(sign_up_realtor_profile_params)
+        user_sign_up_params = sign_up_params.merge(profilable_id: realtor_profile.id)
+      end
+      build_resource(user_sign_up_params)
+      resource.save
+      yield resource if block_given?
+      if resource.persisted?
+        if resource.active_for_authentication?
+          set_flash_message! :notice, :signed_up
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        resource.errors.full_messages.each { |x| flash[:notice] = x }
+      end
     end
 
     protected
 
     def configure_sign_up_params
-      binding.pry
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password avatar first_name last_name second_name date_of_birth gender profilable_type profilable_attributes])
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[user profilable_type first_name last_name second_name city_id gender avatar])
+    end
+
+    def sign_up_client_profile_params
+      params.require(:user).permit(%i[country_id description])
+    end
+
+    def sign_up_realtor_profile_params
+      params.require(:user).permit(%i[registration_number employment_date])
     end
 
     def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: %i[email password avatar first_name last_name second_name date_of_birth gender profilable_type profilable_attributes])
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[user profilable_type first_name last_name second_name city_id gender avatar])
     end
   end
 end
