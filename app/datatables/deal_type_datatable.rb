e71a39c11bbe213d0d469d8242a8dbaf697d4c5b
WhatWasThatt <sashakorol584@mail.ru>
2022-01-15 13:21:06 +0200

    RSA-055/add default datatable for deal type

---
+++ app/datatables/deal_type_datatable.rb
@@ -0,0 +1,25 @@
+class DealTypeDatatable < AjaxDatatablesRails::ActiveRecord
+  def view_columns
+    # Declare strings in this format: ModelName.column_name
+    # or in aliased_join_table.column_name format
+    @view_columns ||= {
+      # id: { source: "User.id", cond: :eq },
+      # name: { source: "User.name", cond: :like }
+    }
+  end
+
+  def data
+    records.map do |_record|
+      {
+        # example:
+        # id: record.id,
+        # name: record.name
+      }
+    end
+  end
+
+  def get_raw_records
+    # insert query here
+    # User.all
+  end
+end
