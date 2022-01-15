a2a05a6f61e789cb9e71c21c9091f0b2d8ed0cfe
WhatWasThatt <sashakorol584@mail.ru>
2022-01-15 13:56:33 +0200

    RSA-055/add model tests for deal type

--- spec/models/deal_type_spec.rb
+++ spec/models/deal_type_spec.rb
@@ -10,5 +10,26 @@
 require 'rails_helper'
 
 RSpec.describe DealType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.deal_type' do
+    let(:deal_type) { create :deal_type }
+    let(:invalid_long) { build :deal_type, :invalid_long }
+    let(:invalid_short) { build :deal_type, :invalid_short }
+    let(:invalid_with_numbers) { build :deal_type, :invalid_with_numbers }
+
+    context 'with valid attributes' do
+      it { expect(deal_type).to be_valid }
+    end
+
+    context 'when too long' do
+      it { expect(invalid_long).not_to be_valid }
+    end
+
+    context 'when too short' do
+      it { expect(invalid_short).not_to be_valid }
+    end
+
+    context 'when with numbers' do
+      it { expect(invalid_with_numbers).not_to be_valid }
+    end
+  end
 end
