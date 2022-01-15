0e6b282e1932ca4f81eec03b5b1361eb4524051d
WhatWasThatt <sashakorol584@mail.ru>
2022-01-15 13:50:37 +0200

    RSA-055/add validation and scope

--- app/models/deal_type.rb
+++ app/models/deal_type.rb
@@ -8,4 +8,7 @@
 #  updated_at :datetime         not null
 #
 class DealType < ApplicationRecord
+  validates :name, length: { in: 3..30 }, format: { with: /\A[a-zA-Z ]+\z/ }
+
+  scope :with_name, ->(name) { where(name: name) }
 end
