4f13bbc8e0da9c1bba8b43281e027e9451215ac8
WhatWasThatt <sashakorol584@mail.ru>
2022-01-19 13:51:20 +0200

    RSA-062/fix address specs

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -21,7 +21,7 @@ require 'rails_helper'
 
 RSpec.describe Address, type: :model do
   describe '.address' do
-    let(:address) { create :addresses }
+    let(:address) { create :address }
 
     context 'with valid attributes' do
       it { expect(address).to be_valid }
@@ -29,9 +29,9 @@ RSpec.describe Address, type: :model do
   end
 
   describe '#building' do
-    let(:invalid_big_building) { build :addresses, :invalid_big_building }
-    let(:invalid_negative_building) { build :addresses, :invalid_negative_building }
-    let(:invalid_with_letters_building) { build :addresses, :invalid_with_letters_building }
+    let(:invalid_big_building) { build :address, :invalid_big_building }
+    let(:invalid_negative_building) { build :address, :invalid_negative_building }
+    let(:invalid_with_letters_building) { build :address, :invalid_with_letters_building }
 
     context 'when too big' do
       it { expect(invalid_big_building).not_to be_valid }
@@ -47,8 +47,8 @@ RSpec.describe Address, type: :model do
   end
 
   describe '#description' do
-    let(:invalid_long_description) { build :addresses, :invalid_long_description }
-    let(:invalid_short_description) { build :addresses, :invalid_short_description }
+    let(:invalid_long_description) { build :address, :invalid_long_description }
+    let(:invalid_short_description) { build :address, :invalid_short_description }
 
     context 'when too long' do
       it { expect(invalid_long_description).not_to be_valid }
