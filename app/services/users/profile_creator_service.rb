da203f5cbaee89b4d40254dbbb45bcd264f44ab4
WhatWasThatt <sashakorol584@mail.ru>
2022-01-19 21:57:22 +0200

    RSA-064/set roles when registering

--- app/services/users/profile_creator_service.rb
+++ app/services/users/profile_creator_service.rb
@@ -8,24 +8,18 @@ module Users
 
     def call
       if profilable_type == 'ClientProfile'
-        client_profile = ClientProfile.create(sign_up_client_profile_params)
-        sign_up_params.merge(profilable_id: client_profile.id)
+        client_profile = ClientProfile.create
+        client_role = Role.client
+        sign_up_params.merge(profilable_id: client_profile.id, role_id: client_role.id)
       else
-        realtor_profile = RealtorProfile.create(sign_up_realtor_profile_params)
-        sign_up_params.merge(profilable_id: realtor_profile.id)
+        realtor_profile = RealtorProfile.create
+        realtor_role = Role.realtor
+        sign_up_params.merge(profilable_id: realtor_profile.id, role_id: realtor_role.id)
       end
     end
 
     private
 
     attr_accessor :profilable_type, :params, :sign_up_params
-
-    def sign_up_client_profile_params
-      params.require(:user).permit(%i[country_id description])
-    end
-
-    def sign_up_realtor_profile_params
-      params.require(:user).permit(%i[registration_number employment_date])
-    end
   end
 end
