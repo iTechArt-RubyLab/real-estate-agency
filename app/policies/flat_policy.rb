f8031270414ec52cd5f5370c5317502559c768bb
WhatWasThatt <sashakorol584@mail.ru>
2022-01-20 18:47:07 +0200

    RSA-065/update lots policies with role permissions

--- app/policies/flat_policy.rb
+++ app/policies/flat_policy.rb
@@ -1,2 +1,51 @@
-class FlatPolicy < DictionaryBasePolicy
+class FlatPolicy < ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    user.role.can_read_lot
+  end
+
+  def show?
+    user.role.can_read_lot
+  end
+
+  def create?
+    user.role.can_edit_lot
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.role.can_edit_lot
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.role.can_edit_lot
+  end
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
 end
