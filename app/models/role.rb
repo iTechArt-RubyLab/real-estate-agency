bb00fd8c098db5120e660ff7c6b2124162b41717
Sasha Karol <70864999+WhatWasThatt@users.noreply.github.com>
2022-01-20 17:29:54 +0300

    RSA-064/create default roles (#105)
    * RSA-064/add default role creating functions
    
    * RSA-064/set client role to users from third party services
    
    * RSA-064/set roles when registering

--- app/models/role.rb
+++ app/models/role.rb
@@ -23,4 +23,46 @@ class Role < ApplicationRecord
   validates :name, length: { in: 3..30 }, format: { with: /\A[a-zA-Z ]+\z/ }
 
   scope :with_name, ->(name) { where(name: name) }
+
+  def self.admin
+    find_or_create_by(name: 'Admin',
+                      can_approve_realtor: true,
+                      can_asign: true,
+                      can_create_role: true,
+                      can_edit_dictionary: true,
+                      can_edit_lot: true,
+                      can_freeze_user: true,
+                      can_give_permission: true,
+                      can_read_dictionary: true,
+                      can_read_lot: true,
+                      can_work_with_all_property_types: true)
+  end
+
+  def self.client
+    find_or_create_by(name: 'Client',
+                      can_approve_realtor: false,
+                      can_asign: false,
+                      can_create_role: false,
+                      can_edit_dictionary: false,
+                      can_edit_lot: true,
+                      can_freeze_user: false,
+                      can_give_permission: false,
+                      can_read_dictionary: false,
+                      can_read_lot: true,
+                      can_work_with_all_property_types: false)
+  end
+
+  def self.realtor
+    find_or_create_by(name: 'Realtor',
+                      can_approve_realtor: false,
+                      can_asign: false,
+                      can_create_role: false,
+                      can_edit_dictionary: false,
+                      can_edit_lot: true,
+                      can_freeze_user: true,
+                      can_give_permission: false,
+                      can_read_dictionary: true,
+                      can_read_lot: true,
+                      can_work_with_all_property_types: false)
+  end
 end
