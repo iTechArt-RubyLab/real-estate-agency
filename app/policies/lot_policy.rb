e03abe05daa00d5a73a9b5f6ad1ca56cf9641ce1
WhatWasThatt <sashakorol584@mail.ru>
2022-01-24 01:53:52 +0200

    RSA-091/add lot policy

---
+++ app/policies/lot_policy.rb
@@ -0,0 +1,32 @@
+class LotPolicy < ApplicationPolicy
+    attr_reader :user, :record
+  
+    def initialize(user, record)
+      @user = user
+      @record = record
+    end
+  
+    def index?
+      user.present?
+    end
+  
+    class Scope
+      def initialize(user, scope)
+        @user = user
+        @scope = scope
+      end
+  
+      def resolve
+        if user.role.can_read_others_lots
+          scope.all
+        else
+          scope.where(client_id: user.id)
+        end
+      end
+  
+      private
+  
+      attr_reader :user, :scope
+    end
+  end
+  
