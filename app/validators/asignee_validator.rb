decc1b794577bd19f1c5c261415466dfc37c5139
WhatWasThatt <sashakorol584@mail.ru>
2022-01-25 22:33:29 +0200

    RSA-105/fix asignee validator

--- app/validators/asignee_validator.rb
+++ app/validators/asignee_validator.rb
@@ -1,5 +1,7 @@
 class AsigneeValidator < ActiveModel::Validator
   def validate(record)
-    record.errors.add(:asignee, 'Asignee have to be realtor') if record.asignee.profilable_type == 'ClientProfile'
+    if !record.asignee.nil?
+      record.errors.add(:asignee, 'Asignee have to be realtor') if record.asignee.profilable_type == 'ClientProfile'
+    end
   end
 end
