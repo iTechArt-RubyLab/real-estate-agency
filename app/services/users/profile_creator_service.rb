336d715b00211f88a4679cf20d25263f44e53c06
Sasha Karol <70864999+WhatWasThatt@users.noreply.github.com>
2022-01-26 04:44:07 +0300

    RSA-100/fix profiles (#182)
    * RSA-100/fix profile creator
    
    * RSA-100/fix edit profile

--- app/services/users/profile_creator_service.rb
+++ app/services/users/profile_creator_service.rb
@@ -8,11 +8,11 @@ module Users
 
     def call
       if profilable_type == 'ClientProfile'
-        client_profile = ClientProfile.create
+        client_profile = ClientProfile.create(description: 'no__info')
         client_role = Role.client
         sign_up_params.merge(profilable_id: client_profile.id, role_id: client_role.id)
       else
-        realtor_profile = RealtorProfile.create
+        realtor_profile = RealtorProfile.create(registration_number: 'no__info', employment_date: Date.current)
         realtor_role = Role.realtor
         sign_up_params.merge(profilable_id: realtor_profile.id, role_id: realtor_role.id)
       end
