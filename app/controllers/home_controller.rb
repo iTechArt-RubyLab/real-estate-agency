63a7e5acb99ec0b5d923ea35016ad760af9829d5
WhatWasThatt <sashakorol584@mail.ru>
2022-01-29 19:57:22 +0200

    RSA-121/update home controller with locales

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,13 @@
 class HomeController < ApplicationController
+  before_action :set_locale
   def index; end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def set_locale
+    locale = params[:locale] || I18n.default_locale
+    I18n.locale = locale.to_sym
+  end
 end
