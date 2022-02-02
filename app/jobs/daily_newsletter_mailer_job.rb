01f5a9f0fb34012618e279314e7bd0d0989bd15e
WhatWasThatt <sashakorol584@mail.ru>
2022-02-02 22:24:34 +0200

    RSA-136/add daily newsletter job

--- app/jobs/daily_newsletter_mailer_job.rb
+++ app/jobs/daily_newsletter_mailer_job.rb
@@ -2,6 +2,8 @@ class DailyNewsletterMailerJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    ser.find_each do |user|
+      DailyNewsletterMailer.with(user: user, fact: DailyNewsletterServices::DailyNewsletter.new.daily_fact).daily_newsletter.deliver_now
+    end
   end
 end
