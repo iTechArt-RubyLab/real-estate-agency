9de4f02da69caed8eb5430c4145053262ce854eb
WhatWasThatt <sashakorol584@mail.ru>
2022-02-04 03:16:46 +0200

    RSA-141/update catalogs with published

--- app/controllers/country_side_houses_catalog_controller.rb
+++ app/controllers/country_side_houses_catalog_controller.rb
@@ -1,5 +1,5 @@
 class CountrySideHousesCatalogController < ApplicationController
   def index
-    @country_side_houses = CountrySideHouse.all
+    @country_side_houses = CountrySideHouse.includes(:lot).where(lot: { state: 'published' })
   end
 end
