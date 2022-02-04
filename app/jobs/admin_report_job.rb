a9ee7e8025cbd4a03ab743d9f305be8b77199bab
WhatWasThatt <sashakorol584@mail.ru>
2022-02-04 23:41:20 +0200

    RSA-150/add reports to admin panel

--- app/jobs/admin_report_job.rb
+++ app/jobs/admin_report_job.rb
@@ -1,7 +1,7 @@
 class AdminReportJob < ApplicationJob
   queue_as :default
 
-  def perform(user, report)
-    ReportMailer.with(user: user).admin_report(report).deliver_now
+  def perform(user, report, report_message)
+    ReportMailer.with(user: user).admin_report(user, report, report_message).deliver_now
   end
 end
--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,7 +1,8 @@
 class ReportMailer < ApplicationMailer
-  def admin_report(report)
+  def admin_report(user, report, report_message)
     @user = params[:user]
     @report = report
-    mail(to: @user.email, subject: "Your report! #{@report}")
+    @report_message = report_message
+    mail(to: @user.email, subject: "Your report about #{@report_message} quantity")
   end
 end
