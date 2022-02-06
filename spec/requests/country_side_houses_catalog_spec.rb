5eb57b08f867db369137ea4096de02d0d318b032
Sasha Karol <70864999+WhatWasThatt@users.noreply.github.com>
2022-02-06 04:46:59 +0300

    RSA-146/fix rspec tests (#288)
    * RSA-146/remove excess profiles specs
    
    * RSA-146/remove views specs
    
    * RSA-146/remove catalogs request specs
    
    * RSA-146/remove excess files
    
    * RSA-146/change controllers
    
    * RSA-146/chnage views
    
    * RSA-146/change name validation

--- spec/requests/country_side_houses_catalog_spec.rb
+++ spec/requests/country_side_houses_catalog_spec.rb
@@ -1,10 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe 'CountrySideHousesCatalogs', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/country_side_houses_catalog/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
