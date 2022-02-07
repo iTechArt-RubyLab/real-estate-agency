62828ede28b4a539c6d19ca496a1eca8eeb72c60
WhatWasThatt <sashakorol584@mail.ru>
2022-02-07 15:24:49 +0200

    RSA-160/change lock permission

--- app/models/role.rb
+++ app/models/role.rb
@@ -66,7 +66,7 @@ class Role < ApplicationRecord
                       can_read_others_lots: true,
                       can_edit_dictionary: false,
                       can_edit_lot: true,
-                      can_freeze_user: true,
+                      can_freeze_user: false,
                       can_give_permission: false,
                       can_read_dictionary: true,
                       can_read_lot: true,
